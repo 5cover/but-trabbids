08ccce8a55e0ce78c9fce919c89f8a0d677cd99c
Scover <>
2025-11-18 16:46:39 +0100

    tf2

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 
 set -eu
 cd "$(dirname "${BASH_SOURCE[0]}")"
-. venv/bin/activate
+# shellcheck source=venv/bin/activate
+. "${1-.venv}"/bin/activate
 set -x
 if ! find data/processed/ -type d -not -empty; then
     python3 -m src.data_loading;
